9f8b447404e2c3efc580425b4839f43d84f80be7
humanpick <beat7586@gmail.com>
2026-03-17 22:00:23 +0900

    Update opp.py

--- opp.py
+++ opp.py
@@ -50,7 +50,7 @@ banner_html = """
 
   <div class="mySlides fade">
     <a href="https://sns7.kr?utm_source=pdf_tool&utm_content=banner2" target="_blank">
-      <img src="https://i.imgur.com/복사한_두번째_이미지주소.jpg">
+      <img src="https://i.imgur.com/qRv31wk.jpg">
     </a>
   </div>
 
